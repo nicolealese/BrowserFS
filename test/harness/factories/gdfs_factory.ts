81e01f28dd09515630a8d2d48f95e626d7613e1b
Nicole Andrews <andre22n@mtholyoke.edu>
2017-07-06 11:57:41 -0400

    refactor factory

--- test/harness/factories/gdfs_factory.ts
+++ test/harness/factories/gdfs_factory.ts
@@ -1,66 +1,58 @@
 import GoogleDriveFileSystem from '../../../src/backend/GoogleDrive';
-import {FileSystem} from '../../../src/core/file_system';
+import {
+    FileSystem
+} from '../../../src/core/file_system';
 
 export default function GDFSFactory(cb: (name: string, obj: FileSystem[]) => void): void {
-  if (GoogleDriveFileSystem.isAvailable()) {
-    var oauthToken: any;
+    if (GoogleDriveFileSystem.isAvailable()) {
+        var oauthToken: any;
+        var req = new XMLHttpRequest();
+        req.open('GET', '/test/fixtures/gdfs/api.js');
+        req.onerror = (e) => {
+            console.error(req.statusText);
+        };
+        req.onload = (e) => {
+            if (!(req.readyState === 4 && req.status === 200)) {
+                console.error(req.statusText);
+            }
+            eval(req.response);
+            onApiLoad();
+        };
+        req.send();
 
-   var req = new XMLHttpRequest();
-    req.open('GET', '/test/fixtures/gdfs/api.js');
-    req.onerror = (e) => { console.log('foo');
-    console.error(req.statusText); };
-    req.onload = (e) => {
-        console.log('bar');
-      if (!(req.readyState === 4 && req.status === 200)) {
-        console.error(req.statusText);
-      }
-      eval(req.response);
-      onApiLoad();
-    };
-    req.send();
+        // Use the API Loader script to load google.picker and gapi.auth.
+        var onApiLoad = () => {
+            // load the APIs
+            gapi.load('client:auth', onAuthApiLoad);
+        };
 
-// loadScript("https://apis.google.com/js/api.js", onApiLoad);
+        var onAuthApiLoad = () => {
+            // The Client ID obtained from the Google Developers Console. Replace with your own Client ID.
+            var clientId = "576255310053-nl3vla4sgg0cmu9ieb3l79fca2iuhrcs.apps.googleusercontent.com"
+            // Scope to use to access user's items.
+            var scope = ['https://www.googleapis.com/auth/drive'];
 
+            ( < any > window).gapi.auth.authorize({
+                    'client_id': clientId,
+                    'scope': scope,
+                    'immediate': false
+                },
+                // log the user in
+                handleAuthResult);
+        };
 
-// Use the API Loader script to load google.picker and gapi.auth.
-var onApiLoad = () => {
-
-    // load the APIs
-    gapi.load('client:auth', onAuthApiLoad);
-};
-
-
-var onAuthApiLoad = () => {
-
-  // The Client ID obtained from the Google Developers Console. Replace with your own Client ID.
-  var clientId = "576255310053-nl3vla4sgg0cmu9ieb3l79fca2iuhrcs.apps.googleusercontent.com"
-
-// Scope to use to access user's items.
-var scope = ['https://www.googleapis.com/auth/drive'];
-
-(<any>window).gapi.auth.authorize({
-    'client_id': clientId,
-    'scope': scope,
-    'immediate': false
-},
-        // log the user in
-        handleAuthResult);
-};
-
-var handleAuthResult = (authResult: any) => {
-    if (authResult && !authResult.error) {
-        oauthToken = authResult.access_token;
-        gapi.client.load('drive', 'v2', () => {
-
-    var fs = new GoogleDriveFileSystem(oauthToken);
-    fs.empty(() => {
-      cb('GoogleDrive', [fs]);
-  });
-        });
+        var handleAuthResult = (authResult: any) => {
+            if (authResult && !authResult.error) {
+                oauthToken = authResult.access_token;
+                gapi.client.load('drive', 'v2', () => {
+                    var fs = new GoogleDriveFileSystem(oauthToken);
+                    fs.empty(() => {
+                        cb('GoogleDrive', [fs]);
+                    });
+                });
+            }
+        };
+    } else {
+        cb('GoogleDrive', []);
     }
-
-};
-} else {
-  cb('GoogleDrive', []);
-}
 }
